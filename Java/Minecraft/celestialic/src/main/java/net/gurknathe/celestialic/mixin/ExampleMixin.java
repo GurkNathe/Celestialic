cd15efd30fcc3dfe852f675888538aa40ff9e796
GurkNathe <ethan.c.krug@gmail.com>
2022-10-19 22:04:59 -0700

    Initial file commit.

---
+++ Java/Minecraft/celestialic/src/main/java/net/gurknathe/celestialic/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.gurknathe.celestialic.mixin;
+
+import net.gurknathe.celestialic.CelestialicMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		CelestialicMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
