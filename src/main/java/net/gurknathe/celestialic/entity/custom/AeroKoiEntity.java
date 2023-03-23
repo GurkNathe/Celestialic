9c08906f489350aade3ef1c54eb6be2ae51ba1b7
GurkNathe <ethan.c.krug@gmail.com>
2023-03-22 17:13:25 -0700

    Added flying to AeroKoiEntity. Needs tweaking.

--- src/main/java/net/gurknathe/celestialic/entity/custom/AeroKoiEntity.java
+++ src/main/java/net/gurknathe/celestialic/entity/custom/AeroKoiEntity.java
@@ -2,21 +2,19 @@ package net.gurknathe.celestialic.entity.custom;
 
 import net.gurknathe.celestialic.particle.CelestialicParticles;
 import net.minecraft.entity.*;
-import net.minecraft.entity.ai.goal.BreatheAirGoal;
+import net.minecraft.entity.ai.TargetPredicate;
+import net.minecraft.entity.ai.control.BodyControl;
+import net.minecraft.entity.ai.control.FlightMoveControl;
+import net.minecraft.entity.ai.control.MoveControl;
 import net.minecraft.entity.ai.goal.Goal;
-import net.minecraft.entity.ai.pathing.EntityNavigation;
-import net.minecraft.entity.ai.pathing.SwimNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.WaterCreatureEntity;
-import net.minecraft.entity.projectile.ShulkerBulletEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.entity.mob.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -27,29 +25,38 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-import java.util.EnumSet;
-import java.util.Random;
+import java.util.*;
 
-public class AeroKoiEntity extends NeutralWaterMob implements IAnimatable {
+public class AeroKoiEntity extends FlyingEntity implements IAnimatable {
 
     private final AnimationFactory factory = new AnimationFactory(this);
+    Vec3d targetPosition;
 
-    public AeroKoiEntity(EntityType<? extends WaterCreatureEntity> entityType, World world) {
+    public AeroKoiEntity(EntityType<? extends FlyingEntity> entityType, World world) {
         super(entityType, world);
+        this.targetPosition = Vec3d.ZERO;
+        this.experiencePoints = 5;
+        this.moveControl = new FlightMoveControl(this, 10, true);
     }
     public static DefaultAttributeContainer.Builder setAttributes() {
         return WaterCreatureEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 14.0)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE,3.0f)
                 .add(EntityAttributes.GENERIC_ATTACK_SPEED, 1.0f)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 4.0f);
+                .add(EntityAttributes.GENERIC_FLYING_SPEED, 4.0f)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.0f);
+    }
+
+
+    protected BodyControl createBodyControl() {
+        return new AeroKoiEntity.AeroKoiBodyControl(this);
     }
 
     @Override
     protected void initGoals() {
         super.initGoals();
-        this.goalSelector.add(2, new AeroKoiEntity.RangedAttackGoal(this));
-        this.goalSelector.add(4, new BreatheAirGoal(this));
+        this.goalSelector.add(5, new AeroKoiEntity.FlyRandomlyGoal(this));
+        this.targetSelector.add(1, new AeroKoiEntity.FindTargetGoal());
     }
 
     /* Gekolib animation controllers */
@@ -60,14 +67,8 @@ public class AeroKoiEntity extends NeutralWaterMob implements IAnimatable {
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-        if (!this.isTouchingWater()) {
-            event.getController().setAnimation(new AnimationBuilder()
-                    .addAnimation("animation.koi.flop", true));
-        } else {
-            event.getController().setAnimation(new AnimationBuilder()
-                    .addAnimation("animation.koi.idle", true));
-        }
-
+        event.getController().setAnimation(new AnimationBuilder()
+                .addAnimation("animation.koi.idle", true));
         return PlayState.CONTINUE;
     }
 
@@ -111,24 +112,11 @@ public class AeroKoiEntity extends NeutralWaterMob implements IAnimatable {
     }
 
     public boolean cannotDespawn() {
+        return false;
+    }
+
+    protected boolean isDisallowedInPeaceful() {
         return true;
-    }
-
-    protected EntityNavigation createNavigation(World world) {
-        return new SwimNavigation(this, world);
-    }
-
-    public void travel(Vec3d movementInput) {
-        if (this.canMoveVoluntarily() && this.isTouchingWater()) {
-            this.updateVelocity(0.01F, movementInput);
-            this.move(MovementType.SELF, this.getVelocity());
-            this.setVelocity(this.getVelocity().multiply(0.9));
-            if (this.getTarget() == null) {
-                this.setVelocity(this.getVelocity().add(0.0, -0.005, 0.0));
-            }
-        } else {
-            super.travel(movementInput);
-        }
     }
 
     public void tickMovement() {
@@ -154,62 +142,97 @@ public class AeroKoiEntity extends NeutralWaterMob implements IAnimatable {
             }
         }
 
-        if (!this.world.isClient) {
-            this.tickAngerLogic((ServerWorld)this.world, true);
-        }
-
         super.tickMovement();
     }
 
-    private static class RangedAttackGoal extends Goal {
+    private class AeroKoiBodyControl extends BodyControl {
+        public AeroKoiBodyControl(MobEntity entity) {
+            super(entity);
+        }
+
+        public void tick() {
+            AeroKoiEntity.this.headYaw = AeroKoiEntity.this.bodyYaw;
+            AeroKoiEntity.this.bodyYaw = AeroKoiEntity.this.getYaw();
+        }
+    }
+
+    private class FlyRandomlyGoal extends MovementGoal {
         private final AeroKoiEntity koi;
-        private int counter;
 
-
-        public RangedAttackGoal(AeroKoiEntity koi) {
+        public FlyRandomlyGoal(AeroKoiEntity koi) {
             this.koi = koi;
-            this.setControls(EnumSet.of(Control.MOVE, Control.LOOK));
         }
 
         public boolean canStart() {
-            LivingEntity livingEntity = this.koi.getTarget();
-            if (livingEntity != null && livingEntity.isAlive()) {
-                return this.koi.getAngryAt() != null;
+            MoveControl moveControl = this.koi.getMoveControl();
+            if (!moveControl.isMoving()) {
+                return true;
             } else {
+                double d = moveControl.getTargetX() - this.koi.getX();
+                double e = moveControl.getTargetY() - this.koi.getY();
+                double f = moveControl.getTargetZ() - this.koi.getZ();
+                double g = d * d + e * e + f * f;
+                return g < 1.0 || g > 3600.0;
+            }
+        }
+
+        public boolean shouldContinue() {
+            return false;
+        }
+
+        public void start() {
+            Random random = this.koi.getRandom();
+            double d = this.koi.getX() + (double)((random.nextFloat() * 2.0F - 1.0F));
+            double e = this.koi.getY() + (double)((random.nextFloat() * 2.0F - 1.0F));
+            double f = this.koi.getZ() + (double)((random.nextFloat() * 2.0F - 1.0F));
+            this.koi.getMoveControl().moveTo(d, e, f, 4.0);
+        }
+    }
+
+    abstract class MovementGoal extends Goal {
+        public MovementGoal() {
+            this.setControls(EnumSet.of(Control.MOVE));
+        }
+
+        protected boolean isNearTarget() {
+            return AeroKoiEntity.this.targetPosition.squaredDistanceTo(AeroKoiEntity.this.getX(), AeroKoiEntity.this.getY(), AeroKoiEntity.this.getZ()) < 4.0;
+        }
+    }
+
+    class FindTargetGoal extends Goal {
+        private final TargetPredicate PLAYERS_IN_RANGE_PREDICATE = TargetPredicate.createAttackable().setBaseMaxDistance(64.0);
+        private int delay = toGoalTicks(20);
+
+        FindTargetGoal() {
+        }
+
+        public boolean canStart() {
+            if (this.delay > 0) {
+                --this.delay;
+                return false;
+            } else {
+                this.delay = toGoalTicks(60);
+                List<PlayerEntity> list = AeroKoiEntity.this.world.getPlayers(this.PLAYERS_IN_RANGE_PREDICATE, AeroKoiEntity.this, AeroKoiEntity.this.getBoundingBox().expand(16.0, 64.0, 16.0));
+                if (!list.isEmpty()) {
+                    list.sort(Comparator.comparing(Entity::getY).reversed());
+                    Iterator var2 = list.iterator();
+
+                    while(var2.hasNext()) {
+                        PlayerEntity playerEntity = (PlayerEntity)var2.next();
+                        if (AeroKoiEntity.this.isTarget(playerEntity, TargetPredicate.DEFAULT)) {
+                            AeroKoiEntity.this.setTarget(playerEntity);
+                            return true;
+                        }
+                    }
+                }
+
                 return false;
             }
         }
 
-        public void start() {
-            this.counter = 20;
-        }
-
-        public void stop() {}
-
-        public boolean shouldRunEveryTick() {
-            return true;
-        }
-
-        public void tick() {
-            if (this.koi.world.getDifficulty() != Difficulty.PEACEFUL) {
-                --this.counter;
-                LivingEntity livingEntity = this.koi.getTarget();
-                if (livingEntity != null) {
-                    this.koi.getLookControl().lookAt(livingEntity, 180.0F, 180.0F);
-                    if (this.koi.squaredDistanceTo(livingEntity) > 25.0) {
-                        if (this.counter <= 25) {
-                            this.counter = 20 + this.koi.random.nextInt(10) * 20 / 2;
-                            // Add ranged attack entity here
-                            this.koi.playSound(SoundEvents.ENTITY_SHULKER_SHOOT, 2.0F, (this.koi.random.nextFloat() - this.koi.random.nextFloat()) * 0.2F + 1.0F);
-                            this.koi.world.spawnEntity(new ShulkerBulletEntity(this.koi.world, this.koi, livingEntity, Direction.Axis.pickRandomAxis(new Random())));
-                        }
-                    } else {
-                        this.koi.setTarget(null);
-                    }
-
-                    super.tick();
-                }
-            }
+        public boolean shouldContinue() {
+            LivingEntity livingEntity = AeroKoiEntity.this.getTarget();
+            return livingEntity != null ? AeroKoiEntity.this.isTarget(livingEntity, TargetPredicate.DEFAULT) : false;
         }
     }
 }
